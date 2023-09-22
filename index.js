32b069bff4fe706fa40dbf0c62f6513f9cbc8f08
faithwest <faithchepkoech425@gmail.com>
2023-09-22 22:48:54 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const employee = {
+  name: 'Sam',
+  streetAddress: '12 Broadway'
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee };
+  
+  updatedEmployee[key] = value;
+
+  return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
 // Write your solution in this file!
